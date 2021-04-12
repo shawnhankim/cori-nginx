f1cf4890e40ded87d7e79b187b37562f1567bf4a
Shawn Kim <h.kim3@f5.com>
2021-04-11 19:56:14 -0700

    feat: sample app

---
+++ oidc/use-case/02-okta-nginx-plus-oidc/03-samples-golang-2.0.0/README.md
@@ -0,0 +1,16 @@
+---
+noteId: "769f45509b3a11ebb0ae0da591b4d39d"
+tags: []
+
+---
+
+# Golang Sample Applications for Okta
+This repository contains several sample applications that demonstrate various Okta use-cases in your Golang application.
+
+Please find the sample that fits your use-case from the table below.
+
+| Sample                                  | Description |
+|-----------------------------------------|-------------|
+| [Okta-Hosted Login](/okta-hosted-login) | A Golang application that will redirect the user to the Okta-Hosted login page of your Org for authentication.  The user is redirected back to the Golang application after authenticating. |
+| [Custom Login Page](/custom-login)      | A Golang application that uses the Okta Sign-In Widget within the Golang application to authenticate the user. |
+| [Resource Server](/resource-server)      | This is a sample API resource server that shows you how to authenticate requests with access tokens that have been issued by Okta. |
---
+++ oidc/use-case/02-okta-nginx-plus-oidc/03-samples-golang-2.0.0/resource-server/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	verifier "github.com/okta/okta-jwt-verifier-golang"
+	oktaUtils "github.com/okta/samples-golang/resource-server/utils"
+)
+
+func main() {
+	oktaUtils.ParseEnvironment()
+
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/api/messages", ApiMessagesHandler)
+
+	log.Print("server starting at localhost:8000 ... ")
+	err := http.ListenAndServe("localhost:8000", nil)
+	if err != nil {
+		log.Printf("the HTTP server failed to start: %s", err)
+		os.Exit(1)
+	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello!  There's not much to see here :) Please grab one of our front-end samples for use with this sample resource server")
+}
+
+func ApiMessagesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, authorization")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, POST,OPTIONS")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	if !isAuthenticated(r) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("401 - You are not authorized for this request"))
+		return
+	}
+
+	m1 := Message{1522272240, "I am a robot."}
+	m2 := Message{1522268640, "Hello, World!"}
+	allMessages := []Message{}
+	allMessages = append(allMessages, m1)
+	allMessages = append(allMessages, m2)
+
+	mess := Messages{
+		allMessages,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(mess)
+
+}
+
+type Message struct {
+	Date float64 `json:"date"`
+	Text string  `json:"text"`
+}
+
+type Messages struct {
+	MessageList []Message `json:"messages"`
+}
+
+func isAuthenticated(r *http.Request) bool {
+	authHeader := r.Header.Get("Authorization")
+
+	if authHeader == "" {
+		return false
+	}
+	tokenParts := strings.Split(authHeader, "Bearer ")
+	bearerToken := tokenParts[1]
+
+	tv := map[string]string{}
+	tv["aud"] = "api://default"
+	tv["cid"] = os.Getenv("SPA_CLIENT_ID")
+	jv := verifier.JwtVerifier{
+		Issuer:           os.Getenv("ISSUER"),
+		ClaimsToValidate: tv,
+	}
+
+	_, err := jv.New().VerifyAccessToken(bearerToken)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
