cc746e2e33ab62675b71e2578a649459785f2259
Shawn Kim <h.kim3@f5.com>
2021-05-16 23:35:37 -0700

    requirement for sample api

--- sample-app/01-oidc-2-domains/01-host-for-user/build-context/api-server/run_sample_goods_app.sh
+++ sample-app/01-oidc-2-domains/01-host-for-user/build-context/api-server/run_sample_goods_app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FLASK_APP=/etc/controller-agent/devportal/apps/sample/goods_mgmt/sample_goods_app.py flask run -h 0.0.0.0 -p 8091
+FLASK_APP=/etc/nginx/api-server/sample_goods_app.py flask run -h 0.0.0.0 -p 8091
